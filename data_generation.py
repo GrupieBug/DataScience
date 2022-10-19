0727737e08d784c06933d31e24a8268d65d668ce
GrupieBug <emkerr00@gmail.com>
2022-10-19 13:25:26 -0600

    EK: fixing l2

--- data_generation.py
+++ data_generation.py
@@ -54,22 +54,48 @@ def sim_logistic_regression(n_features, coefs, n_samples=1000, corr=0.5):
 
 
 def proximal_gradient_l_2(A, b, n_samples, lam, idx, x_star):
-    exponent = - idx * x_star.transpose()
+    exponent = - x_star.transpose() * idx
     sum_l2 = sum(np.log(1 + np.exp(exponent)))
     lam_term = (lam * idx.transpose() * idx)
 
     # l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(-A.transpose() * idx * b))) + (lam * idx.transpose() * idx)
-    l2 = ((1 / (2 * n_samples)) * sum_l2) + lam_term
+    l2_star = ((1 / (2 * n_samples)) * sum_l2) + lam_term
 
-    return l2, min(l2)
+    l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(np.dot(-A.transpose(), b)) * idx)) + (
+                lam * idx.transpose() * idx)
+
+    return l2, l2_star
 
 
 def proximal_gradient_l_1(A, b, n_samples, lam, idx, x_star):
-    exponent = - idx * x_star.transpose()
+    exponent = - x_star.transpose() * idx
     sum_l1 = sum(np.log(1 + np.exp(exponent)))
     lam_term = (lam * idx)
 
     # l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(-A.transpose() * idx * b))) + (lam * idx.transpose() * idx)
+    l1 = ((1 / (2 * n_samples)) * sum_l1) + lam_term
+
+    return l1, min(l1)
+
+
+def heavy_ball(A, b, n_samples, lam, idx, x_star):
+    exponent = - x_star.transpose() * idx
+    sum_l1 = sum(np.log(1 + np.exp(exponent)))
+    lam_term = (lam * idx)
+
+    # l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(-A.transpose() * idx * b))) + (lam * idx.transpose() * idx)
+    l1 = ((1 / (2 * n_samples)) * sum_l1) + lam_term
+
+    return l1, min(l1)
+
+
+def nesterov(A, b, n_samples, lam, idx, x_star):
+    exponent = - x_star.transpose() * idx
+    sum_l1 = sum(np.log(1 + np.exp(exponent)))
+    lam_term = (lam * idx)
+
+    # l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(-A.transpose() * idx * b))) + (lam * idx.transpose() * idx)
+    l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(np.dot(-b, A.transpose()) * idx))) + (lam * idx.transpose() * idx)
     l1 = ((1 / (2 * n_samples)) * sum_l1) + lam_term
 
     return l1, min(l1)
@@ -84,28 +110,38 @@ def main():
     coefs[20:] = 0.
 
     A, b = sim_logistic_regression(n_features, coefs)
-    x_star = A.transpose() * b
-    print(b.transpose())
 
-    lam = 0.001
-    F_l2_steps, l2_min = proximal_gradient_l_2(A, b, n_samples, lam, idx, x_star)
-    F_l1_steps, l1_min = proximal_gradient_l_1(A, b, n_samples, lam, idx, x_star)
+    pseudo_x_first = np.linalg.inv(np.dot(A.transpose(), A))
+    pseudo_x_second = np.dot(A.transpose(), b)
+    x_star = np.dot(pseudo_x_first, pseudo_x_second)
 
-    F_l2_x_star = np.empty(np.shape(F_l2_steps))
-    F_l2_x_star.fill(l2_min)
+    lams = [.001, 0.005, .01, .05, .1]
+    f = plt.figure(1)
+    g = plt.figure(2)
 
-    F_l1_x_star = np.empty(np.shape(F_l1_steps))
-    F_l1_x_star.fill(l1_min)
+    for lam in lams:
+        F_l1_steps, l1_min = proximal_gradient_l_1(A, b, n_samples, lam, idx, x_star)
 
-    x_l2 = idx
-    y_l2 = F_l2_steps - F_l2_x_star
-    plt.plot(x_l2, y_l2)
+        F_l1_x_star = np.empty(np.shape(F_l1_steps))
+        F_l1_x_star.fill(l1_min)
 
-    x_l1 = idx
-    y_l1 = F_l1_steps - F_l1_x_star
-    plt.plot(x_l1, y_l1)
+        x_l1 = idx
+        y_l1 = F_l1_steps - F_l1_x_star
+        plt.plot(x_l1, y_l1)
 
-    plt.show()
+    for lam in lams:
+        F_l2, F_l2_star = proximal_gradient_l_2(A, b, n_samples, lam, idx, x_star)
+
+        diff = F_l2 - F_l2_star
+
+        F_l2_x_star = np.empty(np.shape(F_l2))
+
+        x_l2 = idx
+        plt.plot(x_l2, diff)
+        g.show()
+
+    f.show()
+    g.show()
 
 
 if __name__ == "__main__":
