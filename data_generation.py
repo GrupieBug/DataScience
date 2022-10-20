1644b7fe26b4010dac8b606d0b49758f11c0852e
pmptrck <ptrckpm00@yahoo.com>
2022-10-20 14:07:29 -0600

    v2

--- data_generation.py
+++ data_generation.py
@@ -2,10 +2,12 @@
 # """
 # Data generation for logistic regression
 # """
+from tkinter import X
 import numpy as np
 from numpy.random import multivariate_normal
 from scipy.linalg.special_matrices import toeplitz
 from matplotlib import pyplot as plt
+from scipy.linalg import solve_toeplitz
 
 
 def sigmoid(t):
@@ -37,68 +39,76 @@ def sim_logistic_regression(n_features, coefs, n_samples=1000, corr=0.5):
     return A, b
 
 
-# def F_l_2(A, b, n_samples, lam, idx, x_star):
-#     exponent = - b * A.transpose() * idx
-#     sum_l2 = sum(np.log(1 + np.exp(exponent)))
-#     lam_term = (lam * idx.transpose() * idx)
-#
-#     return ((1 / (2 * n_samples)) * sum_l2) + lam_term
-#
-#
-# def F_l_1(A, b, n_samples, lam, idx, x_star):
-#     exponent = - idx * x_star.transpose()
-#     sum_l1 = sum(np.log(1 + np.exp(exponent)))
-#     lam_term = (lam * idx.transpose() * idx)
-#
-#     return ((1 / (2 * n_samples)) * sum_l1) + lam_term
+def prox_l1(A, b, n_samples, lam, x):
+    idx = 0
+    while (idx < 50):
+        sum_l1_norm = sum(np.log(1 + np.exp(-x)))
+        # lam_term_norm = (lam * x)
+
+        l1 = ((1 / (2 * n_samples)) * sum_l1_norm)  # + lam_term_norm
+        idx += 1
+        return l1
 
 
-def proximal_gradient_l_2(A, b, n_samples, lam, idx, x_star):
-    exponent = - x_star.transpose() * idx
-    sum_l2 = sum(np.log(1 + np.exp(exponent)))
-    lam_term = (lam * idx.transpose() * idx)
+def lasso_regression(A, b, n_samples, lam, x_star):
+    sum_l1 = sum(np.log(1 + np.exp(- x_star)))
+    lam_term = (lam * x_star)
 
-    # l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(-A.transpose() * idx * b))) + (lam * idx.transpose() * idx)
+    l1_star = ((1 / (2 * n_samples)) * sum_l1) + lam_term
+
+    return l1_star
+
+
+def prox_l2(A, b, n_samples, lam, x):
+    sum_l2_norm = sum(np.log(1 + np.exp(-x)))
+    # lam_term_norm = (x.transpose() * lam * x)
+
+    l2 = ((1 / (2 * n_samples)) * sum_l2_norm)  # + lam_term_norm
+
+    return l2
+
+
+def ridge_regression(A, b, n_samples, lam, x_star):
+    sum_l2 = sum(np.log(1 + np.exp(- x_star)))
+    lam_term = (x_star.transpose() * lam * x_star)
+
     l2_star = ((1 / (2 * n_samples)) * sum_l2) + lam_term
 
-    l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(np.dot(-A.transpose(), b)) * idx)) + (
-                lam * idx.transpose() * idx)
-
-    return l2, l2_star
+    return l2_star
 
 
-def proximal_gradient_l_1(A, b, n_samples, lam, idx, x_star):
-    exponent = - x_star.transpose() * idx
-    sum_l1 = sum(np.log(1 + np.exp(exponent)))
-    lam_term = (lam * idx)
+# def proximal_gradient_l_1(A, b, n_samples, lam, idx, x_star):
+#     exponent = - x_star.transpose() * idx
+#     sum_l1 = sum(np.log(1 + np.exp(exponent)))
+#     lam_term = (lam * idx)
 
-    # l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(-A.transpose() * idx * b))) + (lam * idx.transpose() * idx)
-    l1 = ((1 / (2 * n_samples)) * sum_l1) + lam_term
+#     # l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(-A.transpose() * idx * b))) + (lam * idx.transpose() * idx)
+#     l1 = ((1 / (2 * n_samples)) * sum_l1) + lam_term
 
-    return l1, min(l1)
-
-
-def heavy_ball(A, b, n_samples, lam, idx, x_star):
-    exponent = - x_star.transpose() * idx
-    sum_l1 = sum(np.log(1 + np.exp(exponent)))
-    lam_term = (lam * idx)
-
-    # l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(-A.transpose() * idx * b))) + (lam * idx.transpose() * idx)
-    l1 = ((1 / (2 * n_samples)) * sum_l1) + lam_term
-
-    return l1, min(l1)
+#     return l1, min(l1)
 
 
-def nesterov(A, b, n_samples, lam, idx, x_star):
-    exponent = - x_star.transpose() * idx
-    sum_l1 = sum(np.log(1 + np.exp(exponent)))
-    lam_term = (lam * idx)
+# def heavy_ball(A, b, n_samples, lam, idx, x_star):
+#     exponent = - x_star.transpose() * idx
+#     sum_l1 = sum(np.log(1 + np.exp(exponent)))
+#     lam_term = (lam * idx)
 
-    # l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(-A.transpose() * idx * b))) + (lam * idx.transpose() * idx)
-    l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(np.dot(-b, A.transpose()) * idx))) + (lam * idx.transpose() * idx)
-    l1 = ((1 / (2 * n_samples)) * sum_l1) + lam_term
+#     # l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(-A.transpose() * idx * b))) + (lam * idx.transpose() * idx)
+#     l1 = ((1 / (2 * n_samples)) * sum_l1) + lam_term
 
-    return l1, min(l1)
+#     return l1, min(l1)
+
+
+# def nesterov(A, b, n_samples, lam, idx, x_star):
+#     exponent = - x_star.transpose() * idx
+#     sum_l1 = sum(np.log(1 + np.exp(exponent)))
+#     lam_term = (lam * idx)
+
+#     # l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(-A.transpose() * idx * b))) + (lam * idx.transpose() * idx)
+#     l2 = (1 / (2 * n_samples)) * sum(np.log(1 + np.exp(np.dot(-b, A.transpose()) * idx))) + (lam * idx.transpose() * idx)
+#     l1 = ((1 / (2 * n_samples)) * sum_l1) + lam_term
+
+#     return l1, min(l1)
 
 
 def main():
@@ -111,36 +121,43 @@ def main():
 
     A, b = sim_logistic_regression(n_features, coefs)
 
+    c = np.linalg.pinv(A)
+    x = np.dot(c, -b)
+    # print(x)
+
+    # x = np.dot(A.transpose(), -b)
+
     pseudo_x_first = np.linalg.inv(np.dot(A.transpose(), A))
     pseudo_x_second = np.dot(A.transpose(), b)
     x_star = np.dot(pseudo_x_first, pseudo_x_second)
 
-    lams = [.001, 0.005, .01, .05, .1]
+    lams = [.001]  # , .005, .01, .05, .1]
+
     f = plt.figure(1)
     g = plt.figure(2)
 
-    # for lam in lams:
-    #     F_l1_steps, l1_min = proximal_gradient_l_1(A, b, n_samples, lam, idx, x_star)
-    #
-    #     F_l1_x_star = np.empty(np.shape(F_l1_steps))
-    #     F_l1_x_star.fill(l1_min)
-    #
-    #     x_l1 = idx
-    #     y_l1 = F_l1_steps - F_l1_x_star
-    #     plt.plot(x_l1, y_l1)
-
     for lam in lams:
-        F_l2, F_l2_star = proximal_gradient_l_2(A, b, n_samples, lam, idx, x_star)
+        F_l1 = prox_l1(A, b, n_samples, lam, x)
+        F_l1_star = lasso_regression(A, b, n_samples, lam, x_star)
+        diff1 = abs(F_l1 - F_l1_star)  # (BLUE)
 
-        diff = F_l2 - F_l2_star
+        # print(diff1)
+        # print(F_l1)
+        # print(F_l1_star)
 
-        F_l2_x_star = np.empty(np.shape(F_l2))
+        # for lam in lams:
+        F_l2 = prox_l2(A, b, n_samples, lam, x)
+        F_l2_star = ridge_regression(A, b, n_samples, lam, x_star)
+        diff2 = abs(F_l2 - F_l2_star)  # (ORANGE)
 
-        x_l2 = idx
-        plt.plot(x_l2, diff)
-        g.show()
+        # print(diff2)
+        # print(F_l2)
+        # print(F_l2_star)
 
+    plt.plot(idx, diff1)
     f.show()
+
+    plt.plot(idx, diff2)
     g.show()
 
 
